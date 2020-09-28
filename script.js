571ce42d5a591728794959279cf6624edc7ab6a7
kirbycreese <70101203+kirbycreese@users.noreply.github.com>
2020-09-28 10:32:06 -0400

    creating starter html, writing questions in JS

---
+++ script.js
@@ -0,0 +1,10 @@
+var questions = [""]
+//Which of the following is correct about features of JavaScript?
+//Which of the following is a JavaScript reserved word meaning variable? 
+//which of the following refers to a container for multiple types of data values?
+//Which of the following is the type of statement that causes a function to stop executing?
+//Which of the following commands retrieves the length of an array?
+var i;
+for (i = 0; i < questions.length; i++) {
+  text += questions[i] +;
+}
