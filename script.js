798ea4706c181ad57a6d52228e7d10a22177b8f2
kirbycreese <70101203+kirbycreese@users.noreply.github.com>
2020-09-29 13:31:47 -0400

    background image added

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 var questions = ["Which of the following is correct about features of JavaScript?","Which of the following is a JavaScript reserved word meaning variable?","which of the following refers to a container for multiple types of data values?", "Which of the following is the type of statement that causes a function to stop executing?", "Which of the following commands retrieves the length of an array?"];
 console.log(questions)
 
-document.getElementById("startCard").innerHTML = "Hello JavaScript";
+
+
+//document.getElementById("startCard").innerHTML = "Hello JavaScript";
 
 //when start quiz button is clicked, text inside card changes to a countdown timer
 //when start quiz button is clicked, button goes away
