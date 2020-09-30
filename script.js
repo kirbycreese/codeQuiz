1fcedb94c4a4db31d48e6d16720c216946a15151
kirbycreese <70101203+kirbycreese@users.noreply.github.com>
2020-09-30 12:45:54 -0400

    fixed timer bug

--- script.js
+++ script.js
@@ -1,23 +1,23 @@
 var questions = ["Which of the following is correct about features of JavaScript?","Which of the following is a JavaScript reserved word meaning variable?","which of the following refers to a container for multiple types of data values?", "Which of the following is the type of statement that causes a function to stop executing?", "Which of the following commands retrieves the length of an array?"];
 console.log(questions)
-
+var timeLeft = 60;
 
 document.querySelector("#startButton").addEventListener("click",start)
- function start() {
+
+function start() {
  console.log("start")
 
-
-//    // step 2
- //   let question = document.createElement("div") // create div
-//    question.innerHTML = "Some question text!" // set div text
-//    document.querySelector("body").appendChild(question) // just appending to the body tag for now
-//    // step 3
     function timerFunc() {
+      timeLeft = timeLeft - 1;
+      document.getElementById("startCard").innerHTML = "Time Remaining: " + timeLeft;
     }
+
     timer = setInterval(timerFunc, 1000) // define timer
+
  }
- var seconds = Math.floor((timeleft % (1000 * 60)) / 1000);  
- document.getElementById("startCard").innerHTML = "Time Remaining:" + timeleft;
+
+  
+ 
 
 //when start quiz button is clicked, text inside card changes to a countdown timer
 //when start quiz button is clicked, button goes away
