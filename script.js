6f7c049ff5e59bc7f1f3418c84047ca7cf4207b3
kirbycreese <70101203+kirbycreese@users.noreply.github.com>
2020-09-29 22:44:30 -0400

    bug fixes

--- script.js
+++ script.js
@@ -2,7 +2,21 @@ var questions = ["Which of the following is correct about features of JavaScript
 console.log(questions)
 
 
-
+document.querySelector("#startButton").addEventListener("click",start)
+ function start() {
+ console.log("start")
+//       // step 1
+//    document.getElementById("startCard").innerHTML = "Hello JavaScript";
+//    // step 2
+//    let question = document.createElement("div") // create div
+//    question.innerHTML = "Some question text!" // set div text
+//    document.querySelector("body").appendChild(question) // just appending to the body tag for now
+//    // step 3
+//    function timerFunc() {
+//       console.log("I'm here!") // define a placeholder function to execute on every timer interval
+//    }
+//    timer = setInterval(timerFunc, 1000) // define timer
+ }
 //document.getElementById("startCard").innerHTML = "Hello JavaScript";
 
 //when start quiz button is clicked, text inside card changes to a countdown timer
