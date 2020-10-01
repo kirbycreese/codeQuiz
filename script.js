9e728c56a96d53f5052b4230b1ed1c05fc512729
kirbycreese <70101203+kirbycreese@users.noreply.github.com>
2020-10-01 09:54:06 -0400

    working buttons

--- script.js
+++ script.js
@@ -1,25 +1,54 @@
-var questions = ["Which of the following is correct about features of JavaScript?","Which of the following is a JavaScript reserved word meaning variable?","which of the following refers to a container for multiple types of data values?", "Which of the following is the type of statement that causes a function to stop executing?", "Which of the following commands retrieves the length of an array?"];
-console.log(questions)
+var questions = [
+    {
+        question: "Which of the following is correct about features of JavaScript?",
+        choices: ["a","b","c","d"],
+        correct: "c"
+    },
+    {
+        question: "which of the following refers to a container for multiple types of data values?",
+        choices: ["e","f","g","h"],
+        correct: "f"
+    }
+    
+    //"Which of the following is the type of statement that causes a function to stop executing?", 
+    //"Which of the following commands retrieves the length of an array?"
+];
+var currentQuestionIndex = 0
 var timeLeft = 60;
 
 document.querySelector("#startButton").addEventListener("click",start)
 
 function start() {
  console.log("start")
-
+    renderQuestions()
     function timerFunc() {
       timeLeft = timeLeft - 1;
-      document.getElementById("startCard").innerHTML = "Time Remaining: " + timeLeft;
+      document.getElementById("startCard").textContent = "Time Remaining: " + timeLeft;
     }
 
     timer = setInterval(timerFunc, 1000) // define timer
 
- }
+  }
+function renderQuestions(){
+    var showQuestion = document.getElementById("questionsDisplay");
+    var showAnswers = document.getElementById("answersDisplay");
+    showQuestion.textContent = questions[currentQuestionIndex].question
+    var choices = questions[currentQuestionIndex].choices
+    for (var i = 0; i < choices.length; i++){
+        var button = document.createElement("button")
+        button.textContent = choices[i]
+        button.classList.add("btn")
+        button.classList.add("btn-secondary")
+        button.classList.add("mx-2")
+        showAnswers.appendChild(button)
+    }
+}
+
+
 
   
  
 
-//when start quiz button is clicked, text inside card changes to a countdown timer
-//when start quiz button is clicked, button goes away
-document.getElementById("questionsDisplay").innerHTML = "";
-document.getElementById("answersDisplay").innerHTML = "";
+//hide the start
+//give buttons unique class (answer button)
+//on click or event listener for each button, 
